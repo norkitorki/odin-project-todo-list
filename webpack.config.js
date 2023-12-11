bd906a9dfad88ffb004816624a88c9542fbeee6c
norkitorki <norki@tuta.io>
2023-12-11 12:32:26 +0100

    Create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'assets/[hash][ext][query]',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Odin-Project: Todo List',
+    template: './src/home.html',
+    inject: 'head',
+  })],
+};

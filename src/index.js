6c8b700c1b9d95c08bd3e6953011e3f89017e1d9
norkitorki <norki@tuta.io>
2024-04-16 13:34:26 +0200

    Import necessary components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./stylesheets/style.css";
 import './stylesheets/styles.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import { ModalDisplayController   } from './components/modal_display_controller';
+import { ProjectStorage           } from './components/project_storage';
+import { TaskStorage              } from './components/task_storage';
+import { ProjectDisplayController } from './components/project_display';
+import { TaskDisplayController    } from './components/task_display';
+import { DataStorage              } from './components/data_storage';
+import { Alert                    } from './components/alert';
